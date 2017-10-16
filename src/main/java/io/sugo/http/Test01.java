f00b9075ff6dc77b176f04737f7965c1e7d572f8
chenyuzhi459 <553673833@qq.com>
2017-10-16 20:30:24 +0800

    测试interval

--- src/main/java/io/sugo/http/Test01.java
+++ src/main/java/io/sugo/http/Test01.java
@@ -1,15 +1,16 @@
 package io.sugo.http;
 
+import org.joda.time.Interval;
+
 import java.util.*;
 
 public class Test01 {
 
 
     public static void main(String[] args) {
-        String str = "_s";
-        str = str.replace("[","[[]").replace("_","[_]");
-
-        System.out.println("ss:" + str);
+        Interval interval =new Interval(System.currentTimeMillis()-200,System.currentTimeMillis());
+        System.out.println(interval.toString());
+        System.out.println(interval.getEnd().toString("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"));
 
     }
 }
