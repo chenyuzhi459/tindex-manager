ddac53090273f252ec2338d501289e28bed20cdd
chenyuzhi459 <553673833@qq.com>
2017-10-18 17:33:15 +0800

    modify gitignore

--- src/main/java/io/sugo/http/Test01.java
+++ src/main/java/io/sugo/http/Test01.java
@@ -9,8 +9,8 @@ public class Test01 {
 
     public static void main(String[] args) {
         Interval interval =new Interval(System.currentTimeMillis()-200,System.currentTimeMillis());
+        System.out.println("\"end\"");
         System.out.println(interval.toString());
-        System.out.println(interval.getEnd().toString("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"));
 
     }
 }
